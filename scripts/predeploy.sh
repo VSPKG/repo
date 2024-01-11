8c8b78fa97395034f1ef2b8f8443754bc36cb6d9
Vineel Sai <mail@vineelsai.com>
2024-01-11 19:22:22 +0530

    deploy to s3

--- scripts/predeploy.sh
+++ scripts/predeploy.sh
@@ -6,6 +6,7 @@ rm -f docker-compose.yml
 rm -f Dockerfile.*
 rm -f entrypoint.*.sh
 rm vercel.json
+rf .editorconfig
 
 # clean up files that are not needed for deployment on debian repo
 cd linux/debian
